cc087cfd507395a6e3656bc0ae43950f43095de6
AShchurov <ASchurov@appline.ru>
2021-06-10 13:40:55 +0300

    [upd] task 19

--- src/module4/Aircraft.kt
+++ src/module4/Aircraft.kt
@@ -1,9 +1,13 @@
 package module4
 
-class Aircraft(val number: Int, val maxDist: Double, val tank: Double) {
+open class Aircraft(val number: Int, val maxDist: Double, val tank: Double) {
 
     val consumption: Double
         get() {
             return 100 * tank / maxDist
         }
+}
+
+class Boeing747(val passangeres: Int, number: Int, maxDist: Double, tank: Double): Aircraft(number, maxDist, tank){
+
 }
--- src/module4/Main.kt
+++ src/module4/Main.kt
@@ -6,9 +6,8 @@ fun main(args: Array<String>){
     val air2: Aircraft = Aircraft(76543, 9000.0, 9.0)
     val air3: Aircraft = Aircraft(34786, 70000.0, 15.0)
 
-    println("Расход на 100 км для самолёта ${air1.number} = ${air1.consumption}")
-    println("Расход на 100 км для самолёта ${air2.number} = ${air2.consumption}")
-    println("Расход на 100 км для самолёта ${air3.number} = ${air3.consumption}")
+    val boeing: Boeing747= Boeing747(467, 823746, 14310.0, 193.3)
 
+    println("Расход на 100 км для самолёта ${boeing.number} = ${boeing.consumption} тонны топлива")
 
 }
