654781e226a15649874a481f632b49f0ed3cf77a
AShchurov <ASchurov@appline.ru>
2021-06-10 13:29:38 +0300

    [upd] task 18

--- src/module4/Main.kt
+++ src/module4/Main.kt
@@ -1,19 +1,14 @@
 package module4
 
 fun main(args: Array<String>){
-    module4_17()
 
-}
-
-
-fun module4_17(){
     val air1: Aircraft = Aircraft(12345, 6666.0, 6.0)
     val air2: Aircraft = Aircraft(76543, 9000.0, 9.0)
     val air3: Aircraft = Aircraft(34786, 70000.0, 15.0)
 
-    air1.tank = 7.0
-
     println("Расход на 100 км для самолёта ${air1.number} = ${air1.consumption}")
     println("Расход на 100 км для самолёта ${air2.number} = ${air2.consumption}")
     println("Расход на 100 км для самолёта ${air3.number} = ${air3.consumption}")
+
+
 }
