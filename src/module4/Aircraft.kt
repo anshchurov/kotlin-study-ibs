07e10d9b033794f25f772f2b59576268fd71fec7
AShchurov <ASchurov@appline.ru>
2021-06-15 15:35:33 +0300

    [upd] task 21

--- src/module4/Aircraft.kt
+++ src/module4/Aircraft.kt
@@ -1,6 +1,6 @@
 package module4
 
-open class Aircraft(val number: Int, val maxDist: Double, val tank: Double) {
+abstract class Aircraft(val number: Int, val maxDist: Double, val tank: Double) {
 
     val consumption: Double
         get() {
--- src/module4/Main.kt
+++ src/module4/Main.kt
@@ -2,11 +2,7 @@ package module4
 
 fun main(args: Array<String>){
 
-    val air1: Aircraft = Aircraft(12345, 6666.0, 6.0)
-    val air2: Aircraft = Aircraft(76543, 9000.0, 9.0)
-    val air3: Aircraft = Aircraft(34786, 70000.0, 15.0)
-
-    val boeing: Boeing747= Boeing747(467, 823746, 14310.0, 193.3)
+    val boeing: Boeing747= Boeing747(467, 823747, 14310.0, 193.3)
 
     println("В самолете ${boeing.number} ${boeing.passengers} пассажирских мест")
 
